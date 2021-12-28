5e726f14dddd338e572860a680b7a01cccfe68d8
manish <manishpamnani169@gmail.com>
2021-12-28 13:37:56 +0530

    Provide validations in HTML #Start

--- src/App.js
+++ src/App.js
@@ -7,6 +7,39 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    // formErros which will be an object with the input field names
+    // as keys and any validation errors as their values
+    // i.e. email : 'This field is required'
+    // i.e. email : 'Please enter valid email address'
+    
+    this.state = {
+      email : '',
+      password : '',
+      formErrors: {email : '', password : ''},
+      emailValid : false,
+      passwordValid : false,
+      formValid : false
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUserInput(event) {
+    console.log('i am called');
+
+    // name for 'email' , 'password'
+    // value for actual value of the code
+    // so that we don't have to write different code for email & password
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({[name] : value});
+  }
+
+  handleSubmit(event) {
+    // So that page doesn't reloads automatically.
+    event.preventDefault();
   }
 
   render() {
@@ -16,7 +49,7 @@ class App extends React.Component {
           <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control type="email" placeholder="Enter email" value={this.state.email} onChange={(event) => this.handleUserInput(event)}/>
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -24,12 +57,12 @@ class App extends React.Component {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="password" placeholder="Password" value={this.state.password} onChange={(event) => this.handleUserInput(event)}/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" onClick={this.handleSubmit}>
               Submit
             </Button>
           </Form>
